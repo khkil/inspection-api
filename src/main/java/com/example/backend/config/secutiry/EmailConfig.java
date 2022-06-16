a833f855fe863c910b627d6e69827889eecd8dbe
khkil <khkil@micehub.com>
2022-06-16 18:02:43 +0900

    chore: email config 임시 해제

--- src/main/java/com/example/backend/config/secutiry/EmailConfig.java
+++ src/main/java/com/example/backend/config/secutiry/EmailConfig.java
@@ -9,7 +9,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
-@Configuration
+/*@Configuration*/
 public class EmailConfig {
 
     @Value("${mail.smtp.port}")
